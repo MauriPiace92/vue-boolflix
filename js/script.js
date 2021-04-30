3c2b4e3ffcc3bac31f128cf618c3f7f0031e7bd6
MauriPiace92 <75382063+MauriPiace92@users.noreply.github.com>
2021-04-30 10:48:30 +0200

    Fine milestone 2

--- js/script.js
+++ js/script.js
@@ -3,18 +3,21 @@ var root = new Vue(
         el: '#root',
         data: {
             userInput:'',
-            movies:[]
+            movies:[],
+            tvShows:[]
             
         },
         methods: {
             confirmSearch(){
                 
-            
+                // axios for sarching movies:
                 axios
                     .get('https://api.themoviedb.org/3/search/movie?api_key=413470a083cf4dbccd2f6ef0067cf80a',{
 
                         params:{
-                            query:this.userInput
+                            query:this.userInput,
+                            page:1
+
                         }
                     })
                     .then((response)=>{
@@ -22,7 +25,24 @@ var root = new Vue(
 
                         console.log(result.results);
                         this.movies = result.results;
+                    });
+
+                // axios for searching tvshows: 
+                axios
+                    .get('https://api.themoviedb.org/3/search/tv?api_key=413470a083cf4dbccd2f6ef0067cf80a',{
+
+                        params:{
+                            query:this.userInput,
+                            page:1
+
+                        }
                     })
+                    .then((response)=>{
+                        const result = response.data;
+
+                        console.log(result.results);
+                        this.tvShows = result.results;
+                    });
             }
                 
 
